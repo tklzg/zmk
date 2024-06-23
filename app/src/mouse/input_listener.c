751bb009c2facaa9ba3a146fb48cd30f777aff78
tasin <tklzg@naver.com>
2024-06-23 16:02:47 +0900

    Update input_listener.c

--- app/src/mouse/input_listener.c
+++ app/src/mouse/input_listener.c
@@ -151,8 +151,6 @@ static void input_handler(const struct input_listener_config *config,
                           struct input_listener_data *data, struct input_event *evt) {
     // First, filter to update the event data as needed.
 
-    return;
-
     filter_with_input_config(config, evt);
 
     switch (evt->type) {
@@ -173,7 +171,7 @@ static void input_handler(const struct input_listener_config *config,
         }
 
         if (data->mouse.data.mode == INPUT_LISTENER_XY_DATA_MODE_REL) {
-            zmk_hid_mouse_movement_set(data->mouse.data.x, data->mouse.data.y);
+            //zmk_hid_mouse_movement_set(data->mouse.data.x, data->mouse.data.y);
         }
 
         if (data->mouse.button_set != 0) {
@@ -193,8 +191,8 @@ static void input_handler(const struct input_listener_config *config,
         }
 
         zmk_endpoints_send_mouse_report();
-        zmk_hid_mouse_scroll_set(0, 0);
-        zmk_hid_mouse_movement_set(0, 0);
+        //zmk_hid_mouse_scroll_set(0, 0);
+        //zmk_hid_mouse_movement_set(0, 0);
 
         clear_xy_data(&data->mouse.data);
         clear_xy_data(&data->mouse.wheel_data);

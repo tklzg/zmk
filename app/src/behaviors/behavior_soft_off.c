b2108b9ba33442b6aa1ec71df5b413d88b95df66
tasin <tklzg@naver.com>
2025-12-15 21:20:53 +0900

    Revert "Reapply "bluetooth disconnect deep sleep""
    This reverts commit bee6d41dfd7414d2ab50d59f5dae5449fb1f379e.

--- app/src/behaviors/behavior_soft_off.c
+++ app/src/behaviors/behavior_soft_off.c
@@ -9,7 +9,6 @@
 #include <zephyr/device.h>
 #include <drivers/behavior.h>
 #include <zephyr/logging/log.h>
-#include <zmk/split/bluetooth/peripheral.h>
 
 #include <zmk/pm.h>
 #include <zmk/behavior.h>
@@ -25,7 +24,7 @@ struct behavior_soft_off_data {
     uint32_t press_start;
 };
 
-//#define IS_SPLIT_PERIPHERAL                                                                        \
+#define IS_SPLIT_PERIPHERAL                                                                        \
     (IS_ENABLED(CONFIG_ZMK_SPLIT) && !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL))
 
 static int behavior_soft_off_init(const struct device *dev) { return 0; };
